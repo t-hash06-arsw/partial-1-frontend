6aff94383190e260f7e6ee33337ce33710aa7644
t-hash06 <tomasfelipe.p@gmail.com>
2025-09-20 09:08:09 -0500

    feat: import AppProviders for enhanced application structure

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,7 @@ import {
 
 import type { Route } from "./+types/root";
 import "./app.css";
+import { AppProviders } from "./providers";
 
 export const links: Route.LinksFunction = () => [
 	{ rel: "preconnect", href: "https://fonts.googleapis.com" },
