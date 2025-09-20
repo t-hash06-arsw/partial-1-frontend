9d9b7a03f4b9511b3dba8cf3259fecbce7198d14
t-hash06 <tomasfelipe.p@gmail.com>
2025-09-20 09:29:13 -0500

    feat: add form handling and UI components for symbol and function selection

--- app/pages/home.tsx
+++ app/pages/home.tsx
@@ -1,3 +1,6 @@
+import { Input, Select, SelectItem, type SharedSelection } from "@heroui/react";
+import { useForm } from "@tanstack/react-form";
+
 export function meta() {
 	return [
 		{ title: "New React Router App" },
@@ -5,6 +8,92 @@ export function meta() {
 	];
 }
 
+interface FormState {
+	symbol: string;
+	function: SharedSelection;
+	interval: SharedSelection;
+}
+
+const FUNCTIONS = [
+	{ label: "Intraday", value: "TIME_SERIES_INTRADAY" },
+	{ label: "Daily", value: "TIME_SERIES_DAILY" },
+	{ label: "Weekly", value: "TIME_SERIES_WEEKLY" },
+	{ label: "Monthly", value: "TIME_SERIES_MONTHLY" },
+];
+
 export default function Home() {
-	return <h1>Partial</h1>;
+	const form = useForm({
+		defaultValues: {
+			function: new Set(["TIME_SERIES_MONTHLY"]),
+			symbol: "",
+			interval: new Set(["5min"]),
+		} satisfies FormState,
+	});
+
+	return (
+		<main className="bg-content2 w-dvw h-dvh overflow-hidden flex justify-center py-16">
+			<section className="bg-content1 flex flex-col w-full max-w-2xl p-5 shadow-medium rounded-medium h-min">
+				<div className="flex gap-4">
+					<form.Field name="symbol">
+						{(field) => (
+							<Input
+								label="Symbol"
+								placeholder="IBM"
+								labelPlacement="outside"
+								name={field.name}
+								value={field.state.value}
+								onValueChange={field.handleChange}
+							/>
+						)}
+					</form.Field>
+					<form.Field name="function">
+						{(field) => (
+							<Select
+								label="Function"
+								labelPlacement="outside"
+								selectedKeys={field.state.value}
+								onSelectionChange={(value) =>
+									field.handleChange(new Set(value as unknown as string[]))
+								}
+							>
+								{FUNCTIONS.map((func) => (
+									<SelectItem key={func.value}>{func.label}</SelectItem>
+								))}
+							</Select>
+						)}
+					</form.Field>
+					<form.Subscribe
+						selector={(store) =>
+							store.values.function.has("TIME_SERIES_INTRADAY")
+						}
+					>
+						{(isIntraday) =>
+							!isIntraday ? null : (
+								<form.Field name="interval">
+									{(field) => (
+										<Select
+											label="Interval"
+											labelPlacement="outside"
+											selectedKeys={field.state.value}
+											onSelectionChange={(value) =>
+												field.handleChange(
+													new Set(value as unknown as string[]),
+												)
+											}
+										>
+											<SelectItem key="1min">1min</SelectItem>
+											<SelectItem key="5min">5min</SelectItem>
+											<SelectItem key="15min">15min</SelectItem>
+											<SelectItem key="30min">30min</SelectItem>
+											<SelectItem key="60min">60min</SelectItem>
+										</Select>
+									)}
+								</form.Field>
+							)
+						}
+					</form.Subscribe>
+				</div>
+			</section>
+		</main>
+	);
 }
