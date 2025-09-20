e395616c52696ae28e39a1d5ac7d5747b3f42638
t-hash06 <tomasfelipe.p@gmail.com>
2025-09-20 09:09:20 -0500

    feat: configure Tailwind CSS and add global styles for improved theming

---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+// tailwind.config.cjs
+const { heroui } = require("@heroui/react");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		// ...
+		// make sure it's pointing to the ROOT node_module
+		"./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {},
+		colors: {},
+	},
+	darkMode: "class",
+	plugins: [heroui()],
+};
